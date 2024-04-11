2875d7737812ebd2abe48e584cebf7d0df092a91
samridii <161220117+samridii@users.noreply.github.com>
2024-04-11 10:29:56 +0545

    week 1 task 2

--- week1/Hello.java
+++ week1/Hello.java
@@ -2,7 +2,15 @@ package Task.week1;
 
 public class Hello {
     public static void main(String[] args) {
+        /*Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.println("Hello , world!");
-    
+
+
+       /* Extend/Modify the above program to print two additional lines:
+This is my first program
+I am on module ST4003CEM
+System.out.print (not println) and produces the same output */
+        System.out.print( "This is my first program\n" );
+        System.out.printf ("I am  on module ST4003CEM");
     }
 }
